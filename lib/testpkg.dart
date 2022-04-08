34ac1403bfec24632d047d16a5f76df21218567e
ruanjingchun <ruanjingchun@sinochem.com>
2022-04-08 17:54:26 +0800

    test

--- lib/testpkg.dart
+++ lib/testpkg.dart
@@ -1,7 +1,14 @@
 library testpkg;
 
+import 'package:url_launcher/url_launcher.dart';
+
 /// A Calculator.
 class Calculator {
   /// Returns [value] plus 1.
   int addOne(int value) => value + 1;
+
+  void telPhone(String phoneNumer){
+    launch('tel:$phoneNumer');
+  }
+  
 }
